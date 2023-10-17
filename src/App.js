737df9bf8cb2124f680118808f4d62cfcce23a49
Thiago ulrych <ticoulrych@gmail.com>
2023-10-17 08:13:18 -0300

    abc

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-function App() {
-  const [joke, setJoke] = useState('');
+
+const App = () => {
+  const [joke, setJoke] = useState("");
+  const [favorites, setFavorites] = useState(
+    JSON.parse(localStorage.getItem("favorites")) || []
+  );
 
   useEffect(() => {
-  fetch('https://api.chucknorris.io/jokes/random')
-  .then((response) => response.json())
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((response) => response.json())
       .then((data) => setJoke(data.value))
-      .catch((error) => console.error('Erro ao buscar piada:', error));
+      .catch((error) => console.error("Erro ao buscar piada:", error));
   }, []);
+
+  const handleLike = () => {
+    const newFavorites = [...favorites, joke];
+    setFavorites(newFavorites);
+    localStorage.setItem("favorites", JSON.stringify(newFavorites));
+  };
+
+  const handleDelete = (index) => {
+    if (window.confirm("Tem certeza que quer deletar?")) {
+      const newFavorites = favorites.filter((_, i) => i !== index);
+      setFavorites(newFavorites);
+      localStorage.setItem("favorites", JSON.stringify(newFavorites));
+    }
+  };
 
   return (
     <div className="App">
       <h1>Piada do Chuck Norris</h1>
       <p>{joke}</p>
+      <button onClick={handleLike}>Adicionar aos favoritos</button>
+      <h2>Favoritos:</h2>
+      {favorites.map((fav, index) => (
+        <div key={index}>
+          <p>{fav}</p>
+          <button onClick={() => handleDelete(index)}>Deletar</button>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
-
